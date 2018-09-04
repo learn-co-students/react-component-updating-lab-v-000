5ec673e29d551d62764fd3f981fb4772c10acda3
badlychadly <chadmontoya21@yahoo.com>
2018-09-04 13:08:29 -0700

    Done.

--- src/Timer.js
+++ src/Timer.js
@@ -25,11 +25,28 @@ class Timer extends Component {
     clearInterval(this.interval)
   }
 
+
+  shouldComponentUpdate(nextProps, nextState) {
+    // debugger;
+    if (this.state.time === nextState.time) {
+      return false
+    }
+    return true
+  }
+
+
+  componentDidUpdate() {
+    this.timer.current.style.color = '#'+Math.floor(Math.random()*16777215).toString(16)
+    this.timer.current.style.width = 240+this.state.time*5/1000+"px"
+    this.timer.current.style.height = 150+this.state.time*5/1000+"px"
+  }
+
+
   render() {
     const { time, color, className, logText } = this.state
     return (
       <section className="Timer" style={{background: color}} ref={this.timer}>
-
+      
         <h1>{ time }</h1>
         <button onClick={ this.stopClock }>Stop</button>
         <aside className="logText">{ logText }</aside>
