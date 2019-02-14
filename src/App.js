0c447cb549d266bc2dcab867d170cb637d20ceee
Kachel Wittig <kjwittig@gmail.com>
2019-02-14 14:48:29 -0600

    Donezo

--- src/App.js
+++ src/App.js
@@ -1,39 +1,47 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
 
 import Timer from './Timer'
 import Controls from './Controls'
 
 //no need to modify anything in this component
 class App extends Component {
-
   state = {
     updateInterval: 1,
-    timerIDs: []
+    timerIDs: [],
   }
 
+// adds one initial timer
   componentDidMount() {
     this.handleAddTimer()
   }
 
   render() {
-    console.log(this.state.timerIDs);
+    console.log(this.state.timerIDs)
     return (
       <div className="App">
         <header>
           <h1>MultiTimer</h1>
-          <Controls updateIntervalSetting={this.updateIntervalSetting} updateInterval={this.state.updateInterval} handleAddTimer={this.handleAddTimer}/>
+          <Controls
+            updateIntervalSetting={this.updateIntervalSetting}
+            updateInterval={this.state.updateInterval}
+            handleAddTimer={this.handleAddTimer}
+          />
         </header>
-        <div className="TimerGrid">
-          {this.renderTimers()}
-        </div>
-
+        <div className="TimerGrid">{this.renderTimers()}</div>
       </div>
-    );
+    )
   }
 
   // returns array of components written in JSX, mapped from this.state.timerIDs
-  renderTimers = () => this.state.timerIDs.map(({id, updateInterval}) => <Timer key={id} id={id} removeTimer={this.removeTimer} updateInterval={updateInterval}/>)
-
+  renderTimers = () =>
+    this.state.timerIDs.map(({ id, updateInterval }) => (
+      <Timer
+        key={id}
+        id={id}
+        removeTimer={this.removeTimer}
+        updateInterval={updateInterval}
+      />
+    ))
 
   // adds a random number for timer ID
   handleAddTimer = () => {
@@ -42,16 +50,16 @@ class App extends Component {
         ...prevState.timerIDs,
         {
           updateInterval: prevState.updateInterval,
-          id: Date.now()
-        }
-      ]
+          id: Date.now(),
+        },
+      ],
     }))
   }
 
   // removeTimer updates state, removing any timer that matches the provided author
   removeTimer = id => {
     this.setState(prevState => ({
-      timerIDs: prevState.timerIDs.filter(timer => timer.id !== id)
+      timerIDs: prevState.timerIDs.filter(timer => timer.id !== id),
     }))
   }
 
@@ -59,11 +67,10 @@ class App extends Component {
     this.setState(prevState => {
       if (prevState.updateInterval + increment <= 1) return { updateInterval: 1 }
       return {
-        updateInterval: prevState.updateInterval + increment
+        updateInterval: prevState.updateInterval + increment,
       }
     })
   }
-
 }
 
-export default App;
+export default App
