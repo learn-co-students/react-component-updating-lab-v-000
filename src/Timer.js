ab343f1b0fe6bcde15e58365c0208d59bad5e32a
yomi413 <yjescano@gmail.com>
2019-04-17 23:32:08 -0400

    Added code solution

--- src/Timer.js
+++ src/Timer.js
@@ -1,6 +1,6 @@
-import React, { Component } from 'react';
+import React, { PureComponent } from 'react';
 
-class Timer extends Component {
+class Timer extends PureComponent {
 
   constructor() {
     super()
@@ -10,12 +10,17 @@ class Timer extends Component {
         color: '#'+Math.floor(Math.random()*16777215).toString(16),
     }
   }
-
-
-
   //Your code here
+  componentDidUpdate() {
+    this.time.current.stype.color = "#" + Math.floor(Math.random() * 16777215).toString(16);
+  }
 
-
+  shouldComponentUpdate(nextProps, nextState) {
+    if (this.state.time === nextState.time) {
+      return false
+    }
+    return true
+  }
 
   componentDidMount() {
     this.interval = setInterval(this.clockTick, this.props.updateInterval*1000)
