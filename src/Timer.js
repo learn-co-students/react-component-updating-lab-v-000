4ebe707f0561ee5ea3f917efadd857e5cc16ec64
Sarah Paz <sarah.paz@gmail.com>
2019-10-15 21:54:24 -0400

    Done.

--- src/Timer.js
+++ src/Timer.js
@@ -1,6 +1,6 @@
-import React, { Component } from "react";
+import React, { PureComponent } from "react";
 
-class Timer extends Component {
+class Timer extends PureComponent {
   constructor() {
     super();
     this.timer = React.createRef();
@@ -16,12 +16,13 @@ class Timer extends Component {
       "#" + Math.floor(Math.random() * 16777215).toString(16);
   }
 
-  shouldComponentUpdate(nextProps, nextState) {
-    if (this.state.time === nextState.time) {
-      return false;
-    }
-    return true;
-  }
+  //* Changed to PureComponent
+  // shouldComponentUpdate(nextProps, nextState) {
+  //   if (this.state.time === nextState.time) {
+  //     return false;
+  //   }
+  //   return true;
+  // }
 
   componentDidMount() {
     this.interval = setInterval(
